7f64d54c28fedf4f1110a35cb9e9fc88c6968357
AHMET YİĞİT ARITÜRK <l5217085@std.yildiz.edu.tr>
2021-01-25 18:31:42 +0300

    Add project files.

---
+++ Homework4/MyDictionary.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework4
+{
+    class MyDictionary<KeyType , ValueType>
+    {
+        KeyType[] keyTypes;
+        ValueType[] valueTypes;
+        KeyType[] tempKeyTypes;
+        ValueType[] tempValueTypes;
+
+
+        public MyDictionary()
+        {
+            keyTypes = new KeyType[0];
+            valueTypes = new ValueType[0];
+
+        }
+
+        public void Add(KeyType keyType , ValueType valueType)
+        {
+            tempKeyTypes = keyTypes;
+            tempValueTypes = valueTypes;
+
+            keyTypes = new KeyType[keyTypes.Length + 1];
+            valueTypes = new ValueType[valueTypes.Length + 1];
+
+            for (int i = 0; i < tempKeyTypes.Length; i++)
+            {
+                keyTypes[i] = tempKeyTypes[i];
+                valueTypes[i] = tempValueTypes[i];
+            }
+            keyTypes[keyTypes.Length - 1] = keyType;
+            valueTypes[valueTypes.Length - 1] = valueType;
+
+        }
+
+        public int Count
+        {
+            get { return keyTypes.Length; }
+        }
+
+        public ValueType[] Value
+        {
+            get { return valueTypes; }
+        }
+    }
+}
---
+++ Homework4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> sehirler = new Dictionary<int, string>();
+            sehirler.Add(18, "Ankara");
+             Console.WriteLine(sehirler[18]);
+
+
+            MyDictionary<int, string> sehirler2 = new MyDictionary<int, string>();
+            sehirler2.Add(1, "İstanbul");
+            sehirler2.Add(2, "Isparta");
+            Console.WriteLine(sehirler2.Count);
+            Console.WriteLine(sehirler2.Value[1]);
+
+        }
+    }
+}
